ee449c104c5dd80f5b56c9b5c0cd0d5f0fad9232
Prakhar Srivastava <prakhars2558@gmail.com>
2026-03-29 19:33:13 +0530

    updated the monitor.py

--- monitor.py
+++ monitor.py
@@ -5,15 +5,30 @@ import os
 THRESHOLD_UP = 75
 THRESHOLD_DOWN = 30
 
+VM_NAME = "autoscale-vm-3"
+ZONE = "asia-south1-a"
+
 vm_created = False
 
 def scale_up():
     print("Scaling UP → Launching VM")
-    os.system("gcloud compute instances create autoscale-vm-3 --machine-type=e2-micro --image-family=ubuntu-2204-lts --image-project=ubuntu-os-cloud --metadata-from-file startup-script=startup.sh")
+    os.system(f"""
+    gcloud compute instances create {VM_NAME} \
+    --machine-type=e2-micro \
+    --image-family=ubuntu-2204-lts \
+    --image-project=ubuntu-os-cloud \
+    --zone={ZONE} \
+    --tags=http-server \
+    --metadata-from-file startup-script=startup.sh
+    """)
 
 def scale_down():
     print("Scaling DOWN → Deleting VM")
-    os.system("gcloud compute instances delete autoscale-vm-3 --zone=asia-south1-a --quiet")
+    os.system(f"""
+    gcloud compute instances delete {VM_NAME} \
+    --zone={ZONE} \
+    --quiet
+    """)
 
 while True:
     cpu = psutil.cpu_percent(interval=5)
@@ -22,9 +37,11 @@ while True:
     if cpu > THRESHOLD_UP and not vm_created:
         scale_up()
         vm_created = True
+        time.sleep(30)  # wait for VM creation
 
     elif cpu < THRESHOLD_DOWN and vm_created:
         scale_down()
         vm_created = False
+        time.sleep(20)  # avoid rapid delete/recreate
 
     time.sleep(5)
