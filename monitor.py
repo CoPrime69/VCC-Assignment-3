86ce06150fa4eef6a002a1771d1191da54045263
Prakhar Srivastava <prakhars2558@gmail.com>
2026-03-29 19:10:15 +0530

    updated monitor.py to downscale

--- monitor.py
+++ monitor.py
@@ -2,22 +2,29 @@ import psutil
 import time
 import os
 
-THRESHOLD = 75
+THRESHOLD_UP = 75
+THRESHOLD_DOWN = 30
 
-def scale_to_gcp():
-    print("Scaling triggered! Launching VM on GCP")
+vm_created = False
 
-    command = "gcloud compute instances create autoscale-vm-3 --machine-type=e2-micro --image-family=ubuntu-2204-lts --image-project=ubuntu-os-cloud --metadata-from-file startup-script=startup.sh"
+def scale_up():
+    print("Scaling UP → Launching VM")
+    os.system("gcloud compute instances create autoscale-vm-3 --machine-type=e2-micro --image-family=ubuntu-2204-lts --image-project=ubuntu-os-cloud --metadata-from-file startup-script=startup.sh")
 
-    os.system(command)
+def scale_down():
+    print("Scaling DOWN → Deleting VM")
+    os.system("gcloud compute instances delete autoscale-vm-3 --zone=asia-south1-a --quiet")
 
 while True:
-    cpu_usage = psutil.cpu_percent(interval=5)
-    print(f"CPU Usage: {cpu_usage}%")
+    cpu = psutil.cpu_percent(interval=5)
+    print(f"CPU Usage: {cpu}%")
 
-    if cpu_usage > THRESHOLD:
-        print("CPU exceeded threshold!")
-        scale_to_gcp()
-        break
+    if cpu > THRESHOLD_UP and not vm_created:
+        scale_up()
+        vm_created = True
+
+    elif cpu < THRESHOLD_DOWN and vm_created:
+        scale_down()
+        vm_created = False
 
     time.sleep(5)
